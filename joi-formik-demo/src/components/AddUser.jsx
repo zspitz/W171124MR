ebf1c4626652f0446a280ea360e9843578912ad2
Zev Spitz <shivisi@hotmail.com>
2025-04-01 16:01:50 +0300

    Simple Formik

--- joi-formik-demo/src/components/AddUser.jsx
+++ joi-formik-demo/src/components/AddUser.jsx
@@ -1,34 +1,37 @@
 import { useState } from "react"
 import { createUser } from "../services/users"
+import { useFormik } from "formik"
 
 const AddUser = () => {
-    const [name, setName] = useState('')
-    const [isAdmin, setIsAdmin] = useState(false)
+    const formik = useFormik({
+        initialValues: {
+            name: '',
+            isAdmin: false
+        },
+        // validate: (values) => newUserSchema.validate(values),
+        onSubmit: values => {
+            const sendData = async () => {
+                let data
+                try {
+                    data = await createUser(values)
+                    setMsg(JSON.stringify(data))
+                } catch (error) {
+                    setMsg(error.message)
+                }
+            }
+            sendData()
+        }
+    })
 
     const [msg, setMsg] = useState('')
 
-    const submitHandler = e => {
-        e.preventDefault()
-
-        const sendData = async () => {
-            let data
-            try {
-                data = await createUser({ name, isAdmin })
-                setMsg(JSON.stringify(data))
-            } catch (error) {
-                setMsg(error.message)
-            }
-        }
-        sendData()
-    }
-
     return (
         <>
-            <form onSubmit={submitHandler}>
-                <input name="name" type="text" placeholder="Name" value={name}
-                    onChange={e => setName(e.target.value)} />
-                <input type="checkbox" name="isAdmin" checked={isAdmin}
-                    onChange={e => setIsAdmin(e.target.checked)}
+            <form onSubmit={formik.handleSubmit}>
+                <input name="name" type="text" placeholder="Name" value={formik.values.name}
+                    onChange={formik.handleChange} />
+                <input type="checkbox" name="isAdmin" checked={formik.values.isAdmin}
+                    onChange={formik.handleChange}
                 /> Is Admin
                 <button type="submit">Add User</button>
             </form>
