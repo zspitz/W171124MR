38070de26cf0538b3a1394776cd5438522c84591
Zev Spitz <shivisi@hotmail.com>
2025-04-08 04:14:06 +0300

    Better use of Formik's built-in components

--- joi-formik-demo/src/components/AddUser.jsx
+++ joi-formik-demo/src/components/AddUser.jsx
@@ -1,62 +1,77 @@
 import { useState } from 'react';
 import { createUser } from '../services/users';
-import { useFormik } from 'formik';
+import { ErrorMessage, Formik, Form, Field } from 'formik';
 import { newUserSchema } from '../schemas/user';
-import ErrorBox from './ErrorBox';
 
 const AddUser = () => {
-    const formik = useFormik({
-        initialValues: {
-            name: '',
-            isAdmin: false,
-            foo: 'bar'
-        },
-        validate: (values) => {
-            const { error } = newUserSchema.validate(values, { abortEarly: false });
-            if (!error) {
-                return {};
-            }
+    const initialValues = {
+        name: '',
+        isAdmin: false,
+        foo: 'bar'
+    };
 
-            const errors = {};
-            error.details.forEach(detail => {
-                errors[detail.path[0]] = detail.message;
-            });
-            return errors;
-        },
-        onSubmit: values => {
-            const sendData = async () => {
-                let data;
-                try {
-                    data = await createUser(values);
-                    if (data.error?.message) {
-                        setMsg(data.error.message);
-                    } else {
-                        setMsg('User created successfully');
-                    }
-                    console.log(data);
-                } catch (error) {
-                    setMsg(error.message);
-                }
-            };
-            sendData();
+    const validate = (values) => {
+        const { error } = newUserSchema.validate(values, { abortEarly: false });
+        if (!error) {
+            return {};
         }
-    });
+
+        // כדי להציג שגיאות ולידציה שקשורות לכל הטופס, ולא לשדה מסויים.
+        // לדוגמא, המאפיין הנוסף בשם foo
+        setMsg(error.message);
+
+        // פרומט השגיאות שמחזיר ה-Joi אינו תואם את המבנה שה-Formik מצפה לו
+        // צריך למפות מפורמט אחד לשני
+        // תוצאה של validate של Joi: https://joi.dev/api/?v=17.13.3#validationerror
+        // ניתן לראות דוגמא של המבנה ש-Formik מצפה לו כאן: https://formik.org/docs/guides/validation#displaying-error-messages
+        const errors = {};
+        error.details.forEach(detail => {
+            errors[detail.path[0]] = detail.message;
+        });
+        return errors;
+    };
+
+    const onSubmit = (values) => {
+        const sendData = async () => {
+            let data;
+            try {
+                data = await createUser(values);
+                if (data.error?.message) {
+                    setMsg(data.error.message);
+                } else {
+                    setMsg('User created successfully');
+                }
+                console.log(data);
+            } catch (error) {
+                setMsg(error.message);
+            }
+        };
+        sendData();
+    };
+
+    const errStyle = { color: 'red', fontStyle: 'italic' };
+
+    const errorRenderer = err => <span style={errStyle}>{err}</span>;
 
     const [msg, setMsg] = useState('');
 
     return (
         <>
-            <form onSubmit={formik.handleSubmit}>
-                <input name="name" type="text" placeholder="Name" value={formik.values.name}
-                    onChange={formik.handleChange} />
-                <ErrorBox error={formik.errors.name} />
-                <input type="checkbox" name="isAdmin" checked={formik.values.isAdmin}
-                    onChange={formik.handleChange}
-                /> Is Admin
-                <ErrorBox error={formik.errors.isAdmin} />
-                <button type="submit">Add User</button>
-            </form>
-            <p>{msg}</p>
+            <Formik initialValues={initialValues} validate={validate} onSubmit={onSubmit}>
+                {() => (
+                    <Form>
+                        <Field name="name" placeholder="Name" />
+                        <ErrorMessage name="name" render={errorRenderer} />
+                        <br />
+                        <Field name="isAdmin" type="checkbox" /> Is Admin
+                        <ErrorMessage name="isAdmin" render={errorRenderer} />
+                        <br />
+                        <button type="submit">Add User</button>
+                    </Form>
+                )}
+            </Formik>
+            <hr />
+            <p style={errStyle}>{msg}</p>
         </>
     );
 };
--- joi-formik-demo/src/components/ErrorBox.jsx
+++
@@ -1,9 +0,0 @@
-const ErrorBox = ({ error }) => {
-    const style = {
-        height: '1em'
-    };
-    if (error) { style.color = 'red'; }
-    return <div style={style}>{error}</div>;
-};
-
-export default ErrorBox;
